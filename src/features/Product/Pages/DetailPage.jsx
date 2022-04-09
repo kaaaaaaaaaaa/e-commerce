454e90d78fe89aa9219dde4c849de1d5e843f1dd
oanh <oanhhuynh2402@gmail.com>
2022-04-09 21:55:28 +0700

    removed back to top cp in detail page

--- src/features/Product/Pages/DetailPage.jsx
+++ src/features/Product/Pages/DetailPage.jsx
@@ -49,58 +49,7 @@ const useStyles = makeStyles((theme) => ({
     left: '0',
     right: '0',
   },
-  backToTop: {
-    position: 'fixed',
-    bottom: '5rem',
-    right: '10rem',
-  },
 }));
-
-function ScrollTop(props) {
-  const { children, window } = props;
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
-  const trigger = useScrollTrigger({
-    target: window ? window() : undefined,
-    disableHysteresis: true,
-    threshold: 100,
-  });
-
-  const handleClick = (event) => {
-    const anchor = (event.target.ownerDocument || document).querySelector(
-      '#back-to-top-anchor'
-    );
-
-    if (anchor) {
-      anchor.scrollIntoView({
-        behavior: 'smooth',
-        block: 'center',
-      });
-    }
-  };
-
-  return (
-    <Zoom in={trigger}>
-      <Box
-        onClick={handleClick}
-        role="presentation"
-        sx={{ position: 'fixed', bottom: 16, right: 16 }}
-      >
-        {children}
-      </Box>
-    </Zoom>
-  );
-}
-
-ScrollTop.propTypes = {
-  children: PropTypes.element.isRequired,
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window: PropTypes.func,
-};
 
 function DetailPage(props) {
   const classes = useStyles();
@@ -152,15 +101,6 @@ function DetailPage(props) {
 
   return (
     <Box className={classes.root}>
-      <Toolbar id="back-to-top-anchor" />
-      <Box className={classes.backToTop}>
-        <ScrollTop {...props}>
-          <Fab color="primary" size="small" aria-label="scroll back to top">
-            <ExpandLessIcon />
-          </Fab>
-        </ScrollTop>
-      </Box>
-
       <Container>
         <Paper elevation={0}>
           <Grid container>
