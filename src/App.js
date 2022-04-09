53d12be287a4fa33f9c7be9158a8a3770563100c
oanh <oanhhuynh2402@gmail.com>
2022-04-09 21:57:06 +0700

    added back to top function for app

--- src/App.js
+++ src/App.js
@@ -9,10 +9,94 @@ import User from 'features/User';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import theme from './styles/GlobalStyle';
 import { ThemeProvider } from '@material-ui/core/styles';
+import {
+  Box,
+  Fab,
+  Toolbar,
+  makeStyles,
+  Zoom,
+  useScrollTrigger,
+} from '@material-ui/core';
+import PropTypes from 'prop-types';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  // hide tool back To Top
+  backToTopAnchor: {
+    minHeight: '0px !important',
+    [theme.breakpoints.up('sx')]: {
+      minHeight: '0px !important',
+    },
+  },
+  backToTop: {
+    height: '0px !important',
+
+    position: 'fixed',
+    bottom: '5rem',
+    right: '10rem',
+  },
+}));
+function ScrollTop(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector(
+      '#back-to-top-anchor'
+    );
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+      });
+    }
+  };
+
+  return (
+    <Zoom in={trigger}>
+      <Box
+        onClick={handleClick}
+        role="presentation"
+        sx={{ position: 'fixed', bottom: 16, right: 16 }}
+      >
+        {children}
+      </Box>
+    </Zoom>
+  );
+}
+
+ScrollTop.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+function App(props) {
+  const classes = useStyles();
   return (
     <ThemeProvider theme={theme}>
+      {/* ====back to top component ==== */}
+
+      <Toolbar className={classes.backToTopAnchor} id="back-to-top-anchor" />
+      <Box className={classes.backToTop}>
+        <ScrollTop {...props}>
+          <Fab color="primary" size="small" aria-label="scroll back to top">
+            <ExpandLessIcon />
+          </Fab>
+        </ScrollTop>
+      </Box>
+
+      {/* ================================ */}
       <div className="App">
         <Header />
         <Switch>
